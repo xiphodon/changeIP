e54e2447a093e09a14398d62de69446d57eec541
GuoChang <827789895@qq.com>
2018-05-18 18:38:15 +0800

    changeIP_APN

--- app/src/main/java/com/example/topeasecpb/changeip/MyServiceAPN.java
+++ app/src/main/java/com/example/topeasecpb/changeip/MyServiceAPN.java
@@ -32,61 +32,21 @@ public class MyServiceAPN extends AccessibilityService {
                 try {
                     Log.i("runable_targetview", target.toString());
 
-                    AccessibilityNodeInfo _checkBox = getCheckBoxView();
-                    if (_checkBox != null) {
-                        if (_checkBox.isChecked() == false) {
-                            //状态：关
-                            //点击打开
-                            Log.i("状态关，点击打开", _checkBox.toString());
-                            target.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                        } else {
-                            //状态：开
-                            //点击关闭
-                            Log.i("状态开，点击关闭", _checkBox.toString());
-                            target.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-
-                            Thread.sleep(8 * 1000);
-
-                            AccessibilityNodeInfo _checkBox2 = getCheckBoxView();
-                            if (_checkBox2 != null) {
-                                if (_checkBox2.isChecked() == false) {
-                                    //点击打开
-                                    Log.i("状态关，点击打开", _checkBox2.toString());
-                                    target.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                                }
-                            } else {
-                                Log.i("_checkBox2", "_checkBox is null");
-                            }
-
-
-                        }
-
-                    } else {
-                        Log.i("_checkBox", "_checkBox is null");
-                    }
-
-                    Thread.sleep(1500);
-                    AccessibilityNodeInfo _checkBox3 = getCheckBoxView();
-                    if(_checkBox3 != null){
-                        Log.i("操作结束，查看状态", _checkBox3.toString());
-                    }else {
-                        Log.i("操作结束，查看状态", "_checkBox3 is null");
-                    }
+                    target.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    Log.i("选择新APN", target.toString());
 
                     Log.i("div", "==========================================");
 
                 } catch (Exception e) {
                     Log.i("exception", e.toString());
-//                    target = null;
-//                    if(handler != null){
-//                        handler.removeCallbacks(this);
-//                    }
                 }
 
             } else {
                 Log.i("target", "target is null");
 
             }
+
+            target = getTargetView();
 
             handler.postDelayed(this, (long) (zhouqi * 60 * 1000));
         }
@@ -124,7 +84,7 @@ public class MyServiceAPN extends AccessibilityService {
         if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED ||
                 event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED ||
                 event.getEventType() == AccessibilityEvent.TYPE_WINDOWS_CHANGED) {
-            if (className.equals("com.android.phone.settings.MobileNetworkSettings")) {
+            if (className.equals("com.android.settings.Settings$ApnSettingsActivity")) {
                 Log.i("MyService:", "find this activity");
 
                 AccessibilityNodeInfo _target = getTargetView();
@@ -177,21 +137,6 @@ public class MyServiceAPN extends AccessibilityService {
         return super.onUnbind(intent);
     }
 
-    /**
-     * 获取目标checkbox控件
-     *
-     * @return
-     */
-    private AccessibilityNodeInfo getCheckBoxView() {
-        AccessibilityNodeInfo _target = getTargetView();
-        if (_target == null) {
-            return null;
-        }
-        target = _target;
-        Log.i("getCheckBoxView", _target.toString());
-        AccessibilityNodeInfo _checkBoxView = _target.getChild(2);
-        return _checkBoxView;
-    }
 
     /**
      * 获取目标可点击的控件
@@ -204,26 +149,33 @@ public class MyServiceAPN extends AccessibilityService {
         if(nodeInfo != null){
             Log.i("getTargetView", "nodeInfo != null");
             List<AccessibilityNodeInfo> list_button = nodeInfo
-                    .findAccessibilityNodeInfosByText("启用数据网络");
+                    .findAccessibilityNodeInfosByText("中国电信");
 
             for (AccessibilityNodeInfo item : list_button) {
-                AccessibilityNodeInfo parent = item.getParent();
-                if (parent.isClickable()) {
-                    return parent;
+                Log.i("find view:", item.toString());
+                Log.i("isCheckable", item.isCheckable() + "");
+                Log.i("isChecked", item.isChecked() + "");
+                if (item.isCheckable() && !item.isChecked()) {
+                    Log.i("find target view:", item.toString());
+                    AccessibilityNodeInfo parent = item.getParent();
+                    Log.i("target parent view:", parent.toString());
+                    if(parent.isClickable()){
+                        return parent;
+                    }
                 }
             }
 
             return null;
         }else {
-            Log.i("getTargetView", "nodeInfo == null");
-            List<AccessibilityWindowInfo> windowInfos = getWindows();
-
-            for (AccessibilityWindowInfo info : windowInfos){
-                AccessibilityNodeInfo root = info.getRoot();
-                if(root != null){
-                    return root;
-                }
-            }
+//            Log.i("getTargetView", "nodeInfo == null");
+//            List<AccessibilityWindowInfo> windowInfos = getWindows();
+//
+//            for (AccessibilityWindowInfo info : windowInfos){
+//                AccessibilityNodeInfo root = info.getRoot();
+//                if(root != null){
+//                    return root;
+//                }
+//            }
             return null;
         }
     }
